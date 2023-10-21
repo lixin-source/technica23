2d78d2af8b7e386f7872296b4ed549612d59a4b6
Xin Yi Li <61605285+lixin-source@users.noreply.github.com>
2023-10-21 17:47:23 -0400

    Routes added

--- planetplan/src/main.jsx
+++ planetplan/src/main.jsx
@@ -1,10 +1,39 @@
+// Pages
+import App from './App.jsx'
+import NavBar from './routes/NavBar';
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import PrivateRoute from "./pages/PrivateRoute";
+import { UserProvider } from "./contexts/user";
+// import UserProfile from './routes/UserProfile'
+
+import './index.css'
+
+// Route installations
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <UserProvider>
+      <Routes>
+        {/* NavBar */}
+        <Route path="/" element={<NavBar />}/>
+        {/* Home Page */}
+        <Route index={true} path="home" element={<App />} />
+        {/* Sign Up Page */}
+        <Route exact path="/signup" element={<Signup />} />
+        {/* Log In page  */}
+        <Route exact path="/login" element={<Login />} />
+        <Route element={<PrivateRoute />}>
+
+        </Route>
+        {/* User Profile */}
+        {/* <Route index={false} element={UserProfile} /> */}
+      </Routes>
+      </UserProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
---
+++ planetplan/src/routes/TaskDetails.jsx
@@ -0,0 +1,12 @@
+// import TaskDetails from "../pages/TaskDetails";
+
+// const TaskDetails = () => {
+//     return (
+//         <div>
+      
+//             <TaskDetails />
+//       </div>
+//     );
+//   };
+  
+//   export default TaskDetails;
