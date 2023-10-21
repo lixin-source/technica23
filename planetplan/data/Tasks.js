2d32c127852a58de046d983842bdca23087404df
Jennifer Guadarrama <jennyjenn974@gmail.com>
2023-10-21 16:57:08 -0400

    Data

---
+++ planetplan/data/Tasks.js
@@ -0,0 +1,18 @@
+const Tasks =[
+    {
+        "id":1,
+        "title":"Water a Plant",
+        "description":"1. Water Where the Roots Are, 2. Check the Soil Before Watering, 3. Water in the Morning, 4. Water Slowly, 5. Don't Overwater, 6. Use Mulch to Conserve Moisture, 7.  Don’t Let Them Go Dry",
+        "image_task":"https://i.pinimg.com/originals/1f/8e/30/1f8e305de97f4c1bbbad3d26c9b657cd.png",
+    },
+
+    {
+        "id":2,
+        "title":"Exercise for 15 min",
+        "description":"Do 15 min of workout ",
+        "image_task":"https://i.pinimg.com/originals/1f/8e/30/1f8e305de97f4c1bbbad3d26c9b657cd.png",
+    },
+   
+]
+
+export default Tasks
