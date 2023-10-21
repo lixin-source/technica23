db7932506a00da99e55764db2d54ca5cf73a54b2
Jennifer Guadarrama <jennyjenn974@gmail.com>
2023-10-21 17:57:30 -0400

    Create UserProfile.jsx
    Page for the User Profile

---
+++ planetplan/src/pages/UserProfile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UserProfile(){
+    return(
+        <div>
+            <h1>This is User Profile #1</h1>
+        </div>
+    )
+}
+
+export default UserProfile
