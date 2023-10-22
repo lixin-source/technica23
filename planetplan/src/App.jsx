5577c1fc0c1f52d797656130000835041b79afa3
Jennifer Guadarrama <jennyjenn974@gmail.com>
2023-10-22 10:20:21 -0400

    updated App, welcome and home page

--- planetplan/src/App.jsx
+++ planetplan/src/App.jsx
@@ -1,127 +1,111 @@
-//import { useState } from 'react'
+import { useState, useEffect } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
 import '../node_modules/bootswatch/dist/minty/bootstrap.min.css';
-import NavBar from './components/NavBar';
+import { supabase } from './client.js';
 
+import NavBar from './components/NavBar';
 import Task from './components/Task';
 import UserFeed from './components/UserFeed';
 import TopUsers from './components/TopUsers';
-import 'bootswatch/lux/bootstrap.min.css';
-import NavBar from './components/NavBar';
+import UncompletedTasks from './components/UncompletedTasks';
 
 function App() {
 
-  const [tasks, setTasks] = useState([]);
-  const [topUsers, setTopUsers] = useState([]);
-  const [feedData, setFeedData] = useState([]);
+  // const [tasks, setTasks] = useState([]);
+  // const [topUsers, setTopUsers] = useState([]);
+  // const [feedData, setFeedData] = useState([]);
 
-  // Function to fetch and set the tasks
-  const fetchTasks = async () => {
-    try {
-      // Fetch tasks from your database (replace 'tasks' with your actual table name)
-      const { data, error } = await supabase
-        .from('tasks')
-        .select('*')
-        .eq('is_completed', false); // Replace this with your condition for uncompleted tasks
+  // // Function to fetch and set the tasks
+  // const fetchTasks = async () => {
+  //   try {
+  //     // Fetch tasks from your database (replace 'tasks' with your actual table name)
+  //     const { data, error } = await supabase
+  //       .from('tasks')
+  //       .select('*')
+  //       .eq('uncompleted'); // Replace this with your condition for uncompleted tasks
 
-      if (error) {
-        console.error('Error fetching tasks:', error);
-        return;
-      }
+  //     if (error) {
+  //       console.error('Error fetching tasks:', error);
+  //       return;
+  //     }
 
-      setTasks(data);
-    } catch (error) {
-      console.error('Error fetching tasks:', error);
-    }
-  };
+  //     setTasks(data);
+  //   } catch (error) {
+  //     console.error('Error fetching tasks:', error);
+  //   }
+  // };
 
-  useEffect(() => {
-    fetchTasks();
-  }, []);
+  // useEffect(() => {
+  //   fetchTasks();
+  // }, []);
 
-  //Function to fetch top users 
-  const fetchTopUsers = async () => {
-    try {
-      // Fetch top users from your database
-      const { data, error } = await supabase
-        .from('users')
-        .select('*')
-        .order('completed', { ascending: false })
-        .limit(10);
+  // //Function to fetch top users 
+  // const fetchTopUsers = async () => {
+  //   try {
+  //     // Fetch top users from your database
+  //     const { data, error } = await supabase
+  //       .from('users')
+  //       .select('*')
+  //       .order('completed', { ascending: false })
+  //       .limit(10);
 
-      if (error) {
-        console.error('Error fetching top users:', error);
-        return;
-      }
+  //     if (error) {
+  //       console.error('Error fetching top users:', error);
+  //       return;
+  //     }
 
-      setTopUsers(data);
-    } catch (error) {
-      console.error('Error fetching top users:', error);
-    }
-  };
+  //     setTopUsers(data);
+  //   } catch (error) {
+  //     console.error('Error fetching top users:', error);
+  //   }
+  // };
 
-  // Function to fetch and set the user feed data
-  const fetchFeedData = async () => {
-    try {
-      const response = await fetch('https://data/users/feed-data'); // Replace with your API endpoint
-      if (response.ok) {
-        const data = await response.json();
-        setFeedData(data);
-      } else {
-        console.error('Error retrieving data:', response.status);
-      }
-    } catch (error) {
-      console.error('Error retrieving data:', error);
-    }
-  };
+  // // Function to fetch and set the user feed data
+  // const fetchFeedData = async () => {
+  //   try {
+  //     const response = await fetch(''); // Replace with your API endpoint
+  //     if (response.ok) {
+  //       const data = await response.json();
+  //       setFeedData(data);
+  //     } else {
+  //       console.error('Error retrieving data:', response.status);
+  //     }
+  //   } catch (error) {
+  //     console.error('Error retrieving data:', error);
+  //   }
+  // };
 
-  useEffect(() => {
-    fetchTasks();
-    fetchTopUsers();
-    fetchFeedData();
-  }, []);
+  // useEffect(() => {
+  //   fetchTasks();
+  //   fetchTopUsers();
+  //   fetchFeedData();
+  // }, []);
+const dummyID = 5;
 
   return (
     <div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        
-      </div>
-      <p className="read-the-docs">
-        
-      </p>
-    // Home page
+    {/* Home Page */}
     <div id='home'>
       <NavBar />
       {/* Personal todo */}
       <div className="todo-section">
         {/* Fetch from database and show user's tasks that aren't completed */}
-        {tasks.mzp(task=>(
+         {tasks.map(task=>(
           <Task key={task.id} task={task} />
         ))}
+        <UncompletedTasks key={dummyID} id={dummyID} />
       </div>
 
       {/* Fetch people with highest tasks completed as a whole, "greenest thumb" */}
-      <div className="leaderboard">
-        <TopUsers topUsers={topUsers} />
-    // Home page
-    <div id='home'>
-      <NavBar />
-      {/* Personal todo */}
-      <div className="todo-section">
-        {/* Fetch from database and show user's tasks that aren't completed */}
-      </div>
-
-      {/* Fetch people with highest tasks completed as a whole, "greenest thumb" */}
-      <div className="leaderboard">
-      </div>
-
+       <div className="leaderboard">
+        <TopUsers />
+      </div> 
       {/* fetch from user DB and display everything */}
-      <div className="all-users">
-        <UserFeed feedData={feedData} />
-      </div>  
+     <div className="all-users">
+        <UserFeed />
+      </div>   
       </div>  
     </div>
   )
