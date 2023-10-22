879e7efd206bf3f5190a9626e2c427227795240a
Olivia Fratangelo <oliviaf7777@gmail.com>
2023-10-21 20:27:08 -0400

    importing bootstrap theme

--- planetplan/src/App.jsx
+++ planetplan/src/App.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import "bootswatch/dist/[theme]/bootstrap.min.css";
+
 import './App.css'
+import "bootswatch/dist/[theme]/bootstrap.min.css";
 
 function App() {
 
