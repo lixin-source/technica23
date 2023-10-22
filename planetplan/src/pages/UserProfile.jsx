3ca7a122333b76c32e3025ca09e0643ce9618266
Jennifer Guadarrama <jennyjenn974@gmail.com>
2023-10-21 20:35:10 -0400

    Updated Data and User Profile

--- planetplan/src/pages/UserProfile.jsx
+++ planetplan/src/pages/UserProfile.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 
 function UserProfile(){
     return(
-        <div>
-            <h1>This is User Profile #1</h1>
+        <div className='bg-main'>
+            <h1 className='text-center'>User's Profile</h1>
+            <img src="https://img.freepik.com/free-vector/cute-happy-penguin-cartoon-icon-illustration-animal-nature-icon-concept-isolated-flat-cartoon-style_138676-2095.jpg?w=360" className='img-fluid' alt='happy cartoon penguin'/>
+            <p className='.text-success'>Number of tasks completed: </p>
+            <p className='.text-danger'>Number of tasks to be completed: </p>
+            
         </div>
     )
 }
 
-export default UserProfile
+export default UserProfile 
