06d9ee72a4657d0b2db606d0a1acc409aeae3a7e
Olivia Fratangelo <oliviaf7777@gmail.com>
2023-10-21 20:22:13 -0400

    welcome component

--- planetplan/src/App.jsx
+++ planetplan/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+import "bootswatch/dist/[theme]/bootstrap.min.css";
 import './App.css'
 
 function App() {
