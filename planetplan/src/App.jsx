c5a3e9d921e72a76d850226e8799dcea42f02011
Olivia Fratangelo <oliviaf7777@gmail.com>
2023-10-22 09:46:22 -0400

    lux theme

--- planetplan/src/App.jsx
+++ planetplan/src/App.jsx
@@ -2,7 +2,7 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-import 'bootswatch/lux/bootstrap.min.css';
+import 'planetplan/bootswatch/lux/bootstrap.min.css';
 import NavBar from './components/NavBar';
 
 function App() {
